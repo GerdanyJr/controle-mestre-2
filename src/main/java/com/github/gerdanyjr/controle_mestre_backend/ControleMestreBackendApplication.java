fe72fcd3fd234d3bf8dd1432a7b63ba3db062a85
GerdanyJr <asa_lindo@hotmail.com>
2025-07-12 18:32:21 -0300

    first commit

---
+++ src/main/java/com/github/gerdanyjr/controle_mestre_backend/ControleMestreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.controle_mestre_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleMestreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleMestreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gerdanyjr/controle_mestre_backend/ControleMestreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gerdanyjr.controle_mestre_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleMestreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
